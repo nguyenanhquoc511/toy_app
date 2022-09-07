5dc15ab33956a4d2d8c46ad6a9712df0c283a2f3
nguyenanhquoc511 <nguyenanhquoc511@gmail.com>
2022-09-07 13:35:21 +0700

    Feature Microposts, Validate &  Custom display content at user show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -52,6 +52,7 @@ class UsersController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_user
       @user = User.find(params[:id])
+      @contents = @user.microposts.map { |micropost| micropost.content }
     end
 
     # Only allow a list of trusted parameters through.
