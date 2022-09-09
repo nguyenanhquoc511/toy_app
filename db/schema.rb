3bb296760f5ae5d6218d6a55bf56ac6337340ee1
nguyenanhquoc511 <nguyenanhquoc511@gmail.com>
2022-09-09 16:38:18 +0700

    Fix users
    Fix user controller find

---
+++ db/schema.rb
@@ -0,0 +1,9 @@
+ActiveRecord::Schema[7.0].define(version: 2022_09_07_103216) do
+  create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+end
